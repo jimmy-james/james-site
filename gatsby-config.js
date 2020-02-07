88e455382c6719411dd456d8f0b3a1f13ba8105a
jimmy-james <mrjimbot@gmail.com>
2020-02-06 23:14:02 -0500

    fix favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,7 +73,7 @@ module.exports = {
                 background_color: config.backgroundColor,
                 theme_color: config.themeColor,
                 display: `minimal-ui`,
-                icon: `static/favicon.ico`,
+                icon: `static/favicon.png`,
                 legacy: true,
                 query: `
                 {
